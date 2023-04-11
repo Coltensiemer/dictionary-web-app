78871360d9a7dabe33d8c719472964e961c58a09
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-04-11 18:06:24 -0400

    figured out errors for fetch for API

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,12 @@
 import { ChangeEvent, useEffect, useState } from "react";
+import uuid from 'react-uuid';
 
 import PlayIcon from "../assets/images/icon-play.svg";
 import SearchSvg from "../assets/images/icon-search.svg";
 
 export default function Searchbar() {
+	
+	
 // Define the interface for WordData
 interface WordData {
 	id: number;
@@ -19,28 +22,49 @@ interface WordData {
   }
   
 	// Use useState hook to manage state
+
+	const [isError, setError] = useState<boolean>(false)
 	const [isLoaded, setLoaded] = useState(false);
 	const [isData, setData] = useState<WordData[]>([]);
 	const [isWord, setWord] = useState("");
 
   console.log(isWord);
 
-// Define the function that will be called when the user submits the form
-  const submitWord = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault(); // Prevent the default form submission behavior
-    if (isWord.length > 1 && isWord !== "") { // Check if the word is not empty and has more than 1 character
-      async function loadData() { // Define an asynchronous function to fetch the data
-        const response = await fetch(
-          `https://api.dictionaryapi.dev/api/v2/entries/en/${isWord}`
-        );
-        const data = await response.json(); // Convert the response to JSON
-        setData(data); // Update the isData state with the fetched data
-      }
-      loadData(); // Call the asynchronous function to fetch the data
-    }
+// This function handles the form submission
+const submitWord = async (event: React.ChangeEvent<HTMLInputElement>) => {
+	event.preventDefault(); // Prevent the default form submission behavior
+  
+	// Check if the word is empty and set the error state accordingly
+	if (isWord === "") { 
+	  setError(true);
+	} else if (isWord.length > 1 && isWord !== "") { // Check if the word is not empty and has more than 1 character
+	  try {
+		// Make an API request to fetch the word definition
+		const response = await fetch(
+		  `https://api.dictionaryapi.dev/api/v2/entries/en/${isWord}`
+		).then(handleError); // Handle errors in the response using the handleError function
+  
+		// Convert the response to JSON and update the data state with the fetched data
+		const data = await response.json();
+		setData(data);
+		
+		// Set the error state to false to render the definition component
+		setError(false);
+	  } catch (error) { // Catch errors that occur during the API request
+		console.log("error", error);
+	  }
+	} 
   };
-
-
+  
+  // This function handles errors in the response
+  function handleError(response: Response) { 
+	if (!response.ok) { // If the response is not ok, throw an error with the status text
+	  console.log("error");
+	  throw Error(response.statusText);
+	}
+	return response;
+  }
+  
   return (
     <div>
       <div className="pb-10">
@@ -56,10 +80,11 @@ interface WordData {
             <img src={SearchSvg} className="absolute right-10 top-[100px]" />
           </button>
         </form>
+		{ isError == true && <p className="text-red-primary">Whoops, can't be empty</p> } 
       </div>
       <ul>
         {isData.map((data) => (
-          <li key={data.id}>
+          <li key={uuid()}>
             <div className="flex justify-between">
               <h2 className="text-3xl md:text-6xl">{data.word}</h2>
               <img className="" src={PlayIcon} />
@@ -70,7 +95,7 @@ interface WordData {
 
             <ul>
               {data.meanings.map((meaning) => (
-                <li key={meaning.id}>
+                <li key={uuid()}>
                   <h3 className="text-xl font-bold  italic ">
                     {meaning.partOfSpeech}
                   </h3>
@@ -89,7 +114,7 @@ interface WordData {
                         <h4 className="pr-6 text-grey-primary">Synonyms</h4>
                       )}
                       {meaning.synonyms.map((sym) => (
-                        <ul key={sym}>
+                        <ul key={uuid()}>
                           <li className="text-purple-primary">{sym}</li>
                         </ul>
                       ))}
