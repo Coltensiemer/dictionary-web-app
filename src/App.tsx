d61065e568f3146e1a1167fc3787adc6f40a6bf9
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-04-13 21:21:27 -0400

    started working on props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Search from './components/Search'
-import './styles/App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import "./styles/App.css";
 
 function App() {
-  
+  const [isFontFamily, setFontFamily] = useState<string>("sans serif");
+
+  function handleFontFamily(select: string) {
+    setFontFamily(select);
+  }
 
   return (
     <div className="flex flex-col justify-center mx-6">
-      <Navbar />
+      <Navbar handleFontFamily={handleFontFamily} isFontFamily={isFontFamily} />
       <Search />
-  
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,84 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Logo from "../assets/images/logo.svg";
 import Moon from "../assets/images/icon-moon.svg";
 import DownArrow from "../assets/images/icon-arrow-down.svg";
 
-// the light and dark mode toggle switch
-export function LightDarkToggle() {
-  return (
-    <div className="flex gap-2">
-      <input type="checkbox" className="toggle"  />
-      <img className="relative bottom-1" src={Moon} />
-    </div>
-  );
+interface Props { 
+  handleFontFamily: (select: string) => void
+  isFontFamily: string; 
 }
 
-// Fonts - will control drop downs
-export function Font() {
-  return (
-    <div className="flex justify-around gap-4">
-      <p>Serif</p>
-      <img src={DownArrow} className="h-2 relative top-2" />
-      <div className="text-grey-medium">|</div>
-      <LightDarkToggle />
-    </div>
-  );
-}
+export default function Navbar(props: Props) {
+  const [isOpen, setOpen] = useState<boolean>(false);
+ 
 
-export default function Navbar() {
+
+
+  function DropDown() {
+  
+    return (
+      <div className="absolute z-10 mt-8 p-6  bg-white shadow-xl transition ease-in-out duration-700 ">
+        <ol className=" self-start flex flex-col gap-2">
+          <li
+            onClick={() => props.handleFontFamily("sans serif")}
+            value="Sans Serif"
+            className="hover:text-purple-primary font-sans "
+          >
+            Sans Serif
+          </li>
+          <li
+            onClick={() => props.handleFontFamily("serif")}
+            value="Serif"
+            className="hover:text-purple-primary  font-serif "
+          >
+            Serif
+          </li>
+          <li
+            onClick={() => props.handleFontFamily("monospace")}
+            value="Mono"
+            className="hover:text-purple-primary font-mono"
+          >
+            Mono
+          </li>
+        </ol>
+      </div>
+    );
+  }
+
+  // the light and dark mode toggle switch
+  function LightDarkToggle() {
+    return (
+      <div className="flex gap-2">
+        <input type="checkbox" className="toggle" />
+        <img className="relative bottom-1" src={Moon} />
+      </div>
+    );
+  }
+
+  // Fonts - will control drop downs
+  function Font() {
+    return (
+      <div className="flex justify-around gap-4">
+        <div className="w-32 flex flex-col">
+          <div className="flex self-end gap-1">
+            <p>{props.isFontFamily}</p>
+            <button onClick={() => setOpen(!isOpen)}>
+              <img src={DownArrow} className="h-2 relative" />
+            </button>
+          </div>
+          {isOpen && <DropDown />}
+        </div>
+        <div className="text-grey-medium">|</div>
+        <LightDarkToggle />
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-between my-6">
       <img className="h-8 w-8" src={Logo} />
       <Font />
+      <p>CHANGE ME</p>
     </div>
   );
 }
